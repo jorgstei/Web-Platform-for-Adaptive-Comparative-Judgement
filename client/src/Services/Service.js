17a314dc7ff80cba8d2c722163d27d789dce59a3
Lasse Seivaag <lasse_0407@hotmail.com>
2021-01-26 20:10:50 +0100

    Add service class for ComparisonObject, create simple test page for get all, get by id and get random pair

---
+++ client/src/Services/Service.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+export default class Service{
+    path = "http://localhost:3000/api"
+}
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,9 @@
 import App from './App.svelte';
 import Landing_page from './Landing_page.svelte'
 import LoginPage from './LoginPage.svelte'
+import ComparisonObjectServiceTest from "./ComparisonObjectServiceTest.svelte"
 
-const app = new LoginPage({
+const app = new ComparisonObjectServiceTest({
 	target: document.body,
 	props: {
 		studyTitle: "Eksamensoppgave 3b"
